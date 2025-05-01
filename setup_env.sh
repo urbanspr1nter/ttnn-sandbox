86bee710968f2f971d669600a69fd8266eb05f0d
Roger Ngo <rngo@netflix.com>
2025-05-01 04:58:34 -0700

    edit

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install torch==2.2.1+cpu --index-url https://download.pytorch.org/whl/cpu
+pip install torch==2.2.1 --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
 
 echo "✅ Successfully installed ttnn library"
