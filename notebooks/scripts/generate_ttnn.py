6227e5b4cefb417ae0c66c3248b16f04cf67ec5d
Roger Ngo <urbanspr1nter@gmail.com>
2025-05-12 15:02:52 -0700

    making it work

--- notebooks/scripts/generate_ttnn.py
+++ notebooks/scripts/generate_ttnn.py
@@ -3,23 +3,74 @@ import torch
 import tiktoken
 from torch import nn
 
+def manual_argmax_ttnn(
+  tensor, dim=-1, keepdim=True, device=None
+):
+  """
+  Super inefficient
+  """
+  torch_tensor = ttnn.to_torch(tensor)
+  result = torch.argmax(
+    torch_tensor,
+    dim=dim,
+    keepdim=keepdim
+  )
+
+  result_ttnn = ttnn.from_torch(
+    result,
+    dtype=ttnn.uint32,
+    device=device
+  )
+
+  return result_ttnn
+
+def manual_concat(ttnn_1, ttnn_2, dim=-1, device=None):
+  """
+  Super inefficient
+  """
+
+  result_torch = torch.cat((
+    ttnn.to_torch(ttnn_1),
+    ttnn.to_torch(ttnn_2)
+  ), dim=dim)
+
+  result_ttnn = ttnn.from_torch(
+    result_torch,
+    dtype=ttnn.uint32,
+    device=device
+  )
+
+  return result_ttnn
+
 def generate_text_simple_ttnn(model, idx_ttnn, max_new_tokens, context_size, device):
-  """
-  Note: this could be more efficient. im moving things around waaay too much here between
-  CPU and TPU.
-  """
-  idx = ttnn.to_torch(idx_ttnn, device=device)
   for _ in range(max_new_tokens):
-    idx_cond = idx[:, -context_size:]
+    if idx_ttnn.shape[1] < context_size:
+      idx_cond = idx_ttnn[:, :idx_ttnn.shape[1]]
+    else:
+      idx_cond = idx_ttnn[:, -context_size:]
 
-    with torch.no_grad():
-      logits_ttnn = model(ttnn.from_torch(idx_cond, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device))
-      logits = ttnn.to_torch(logits_ttnn, device=device)
+    logits_ttnn = model(idx_cond)
+    logits_ttnn = logits_ttnn[:, -1, :]
+    probas_ttnn = softmax_ttnn(logits_ttnn)
 
-    logits = logits[:, -1, :]
-    probas = torch.softmax(logits, dim=-1)
-    idx_next = torch.argmax(probas, dim=-1, keepdim=True)
-    
-    idx = torch.cat((idx, idx_next), dim=1)
+    idx_next = manual_argmax_ttnn(
+      probas_ttnn,
+      dim=-1,
+      keepdim=True,
+      device=device
+    )
 
-  return idx
+    idx_ttnn = manual_concat(idx_ttnn, idx_next, dim=1, device=device)
+
+  print(idx_ttnn)
+  return idx_ttnn[0]
+
+
+def softmax_ttnn(x_ttnn):
+  return ttnn.div(
+    ttnn.exp(x_ttnn),
+    ttnn.sum(
+      ttnn.exp(x_ttnn),
+      dim=-1
+    )
+  )
