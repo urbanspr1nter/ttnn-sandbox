fcc159674d79e29f50cde786acea6991fb06be84
Roger Ngo <urbanspr1nter@gmail.com>
2025-05-14 21:01:39 -0700

    Finally done with the pretraining notebook

--- notebooks/scripts/generate_ttnn.py
+++ notebooks/scripts/generate_ttnn.py
@@ -1,33 +1,29 @@
 import ttnn
 import torch
 
-def argmax_and_concat(x_ttnn, probas_ttnn, device=None):
+def argmax_and_concat_ttnn(x_ttnn, probas_ttnn, device=None):
   """
   Unfortunately this is super inefficient. I need to figure out how
-  we can offload all this to ttnn
+  we can offload all this to ttnn. Right now something is up with concat.
   """
-
-  x_torch = ttnn.to_torch(x_ttnn)
-  probas_torch = ttnn.to_torch(probas_ttnn)
-
-  max_arg_idx = torch.argmax(
-    probas_torch,
-    dim=-1,
-    keepdim=True
-  )
   
-  result_torch = torch.cat((
-    x_torch,
-    max_arg_idx 
+  probas_ttnn = ttnn.to_layout(probas_ttnn, layout=ttnn.ROW_MAJOR_LAYOUT, device=device)
+  probas_ttnn = ttnn.reshape(probas_ttnn, (1, 1, probas_ttnn.shape[0], probas_ttnn.shape[1]))
+  
+  max_arg_idx = ttnn.argmax(
+    probas_ttnn,
+    dim=-1
+  )
+  # Ensure max_arg_idx has the same dtype as x_ttnn
+  max_arg_idx = ttnn.reshape(max_arg_idx, (1, 1))
+
+  result_torch = torch.concat((
+    ttnn.to_torch(x_ttnn),
+    ttnn.to_torch(max_arg_idx)
   ), dim=-1)
 
-  result_ttnn = ttnn.from_torch(
-    result_torch,
-    layout=ttnn.TILE_LAYOUT,
-    dtype=ttnn.uint32,
-    device=device
-  )
-
+  result_ttnn = ttnn.from_torch(result_torch,dtype=ttnn.uint32, device=device)
+  
   return result_ttnn
 
 def generate_text_simple_ttnn(model, idx_ttnn, max_new_tokens, context_size, device):
@@ -41,8 +37,8 @@ def generate_text_simple_ttnn(model, idx_ttnn, max_new_tokens, context_size, dev
     logits_ttnn = logits_ttnn[:, -1, :]
     probas_ttnn = softmax_ttnn(logits_ttnn)
 
-    # this is still being done on CPU
-    idx_ttnn = argmax_and_concat(idx_ttnn, probas_ttnn, device=device)
+    # this is still being done on CPU during concat
+    idx_ttnn = argmax_and_concat_ttnn(idx_ttnn, probas_ttnn, device=device)
 
   return idx_ttnn
 
